85ada61d653294a424d64776ce26cf601b5e7bda
Patel Keyur Chiragbhai <99000773+21BCA112@users.noreply.github.com>
2022-03-15 13:55:43 +0530

    program 17 uploaded

---
+++ String/check panlidrome.py
@@ -0,0 +1,12 @@
+a=input("enter any sentence:")
+b=[]
+for i in a.split():
+    if(i==i[::-1]):
+        b.append(i)
+print()
+for i in range(len(b)):
+       print(b[i])
+print()
+print("Total {} panlidrome words in string:{}".format(len(b),b))
+      
+        
